bc82a69b4923a7eace74096fc3fbab0d78d592f7
CHOIY3SUNG <choiyesung2848@naver.com>
2022-08-22 14:50:47 +0900

    Initial Commit

---
+++ first/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  자료구조
+//
+//  Created by Y3SUNG on 2022/08/22.
+//
+
+#include <stdio.h>
+
+int main() {   // void로 하면 경고가 뜬다.
+    
+    int age = 18;
+    float height = 187.0;
+    char pass = 'T';
+    
+    printf("age    =  %d \n", age);
+    printf("height =  %f \n", height);
+    printf("pass   =  %c \n", pass);
+    
+    return 0;
+}
